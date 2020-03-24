38aa74ee3fc8fb70978f66000888767d61b498c6
damilare anjorin <damilareanjorin1@gmail.com>
2020-03-24 18:27:51 +0100

    personal website

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -10,15 +10,16 @@ class Experience extends Component {
                             <div className="exp-heading">  
                                 <h3>Over the past 2 years,</h3> 
                                 <p className="description">
-                                I've built products for companies and businesses around the globe ranging from basic websites to complex solutions and enterprise apps with focus on fast, elegant and accessible user experiences.
-
-Most recently, I was Principal Frontend Software Engineer at hellotax crafting a suite of tools and services tailored at providing robust and fast VAT Registration & Returns solutions for all kinds of traders across Europe.
-
-Before then, I was a Senior frontend engineering contractor with Pixel2HTML building JavaScript applications and interfaces for orgs and individuals.
-
-I once also led the frontend team at a Russian startup, Conectar through building multiple React applications into a single powerful online learning platform.
-
-I'm now available for contracts and remote full-time roles. Want us to work together? You should contact me.
+                                I've built projects ranging from basic websites to complex solutions using web technologies with focus on fast, elegant and responsive.
+                                </p>
+                                <p className="description">
+                                Recently, I led the fullstack web developer team, where i worked remotely on a contract basis to develop worshipper in me website crafting a suite of tools and services tailored at providing robust and fast to aid performance.
+                                </p>
+                                <p className="description">
+                                I once also worked as a web developer intern at Lagos state university where i led the backend team through building multiple applications.
+                                </p>
+                                <p className="description">
+                                I'm now available for contracts and remote full-time roles. Want us to work together? You should <a href="#">contact me...</a>
                                 </p>
                                 
                             </div>
